e4bfcee2379d6a094b98912742a1576e650883ce
egzontina <egzontina.krasniqi@gmail.com>
2017-11-01 02:15:45 +0100

    Search filter is added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 
+import { FormsModule } from '@angular/forms';
 import { LokiService } from './loki/loki.service';
 import { HttpModule } from '@angular/http';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NgxPaginationModule } from 'ngx-pagination';
   imports: [
     BrowserModule,
     HttpModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   providers: [LokiService],
   bootstrap: [AppComponent]
